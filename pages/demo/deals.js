2a8a0572525f9d1af06021ba16f747c0b4640bd1
sharad-pizza-hut <sharad.shekar@yum.com>
2020-01-29 09:51:33 -0600

    Image SEO on /deals

--- pages/demo/deals.js
+++ pages/demo/deals.js
@@ -1,19 +1,37 @@
-import React, { useEffect, Fragment } from 'react'
+import React, { useEffect, Fragment } from 'react';
 
 // SEO
 import { NextSeo } from 'next-seo';
 
-const Deals = () => {
-    useEffect(() => {
-        // Force redirect
-        window.location.href = "https://www.pizzahut.com/link.php?menu=deals"
-    });
-    return (
-        <Fragment>
-            <NextSeo title="Pizza Hut | Deals" description="Pizza Hut | Deals | NextSEO" />
-        </Fragment>
-    )
-}
+const Deals = ({ seo_image }) => {
+  useEffect(() => {
+    // Force redirect
+    window.location.href = 'https://www.pizzahut.com/link.php?menu=deals';
+  });
+  return (
+    <Fragment>
+     <p> Redirecting... </p>
+      <NextSeo
+        title="Pizza Hut | Deals"
+        description="Pizza Hut | Deals | NextSEO"
+        openGraph={{
+          images: [
+            {
+              url: seo_image,
+              alt: 'Pizza Hut Deals Image'
+            }
+          ]
+        }}
+      />
+    </Fragment>
+  );
+};
 
+Deals.getInitialProps = ctx => {
+  const getURL = () =>
+    'https://www.pizzahut.com/assets/w/images/homepage_deal/PH_Local+Deal_Sidekick_730x320.jpg';
+  const seo_image = getURL();
+  return { seo_image };
+};
 
-export default Deals
+export default Deals;
